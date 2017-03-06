862bdc4e64ef681544a50661723652481c60d354
Bruckner, Alexander <Alexander.Bruckner@zuehlke.com>
2017-03-06 15:00:59 +0100

    fixed with angular-ui-router

--- modules/angular2-test/src/main/resources/META-INF/resources/js/head.js
+++ modules/angular2-test/src/main/resources/META-INF/resources/js/head.js
@@ -1,22 +1,31 @@
 function bootstrapAngular(portletId) {
 
-	angular.module(portletId, ['ngRoute'])
-	  .config(function($routeProvider) {
-		    $routeProvider
-			    .when("/" + portletId + "/route1", {
-			        templateUrl : "/o/angular2test/html/route1.html",
-			        controller  : 'Route1Controller' 
-			    })
-			    .when("/" + portletId + "/route2", {
-			        templateUrl : "/o/angular2test/html/route2.html",
-			        controller  : 'Route2Controller' 
-			    })
-		    
-		})
-	  .controller('MyController', ['$scope', '$location', function ($scope, $location) {
+	angular.module(portletId, ['ui.router'])
+	   .config(['$locationProvider', '$stateProvider', function ($locationProvider, $stateProvider) {
+	        $stateProvider
+	        .state('route1', {
+	            url: '/' + portletId + '/route1',
+	            views: {
+	                'view': {
+	                    templateUrl: '/o/angular2test/html/route1.html',
+	                    controller  : 'Route1Controller'
+	                }
+	            }
+	        })
+	        .state('route2', {
+	            url: '/' + portletId + '/route2',
+	            views: {
+	                'view': {
+	                    templateUrl: '/o/angular2test/html/route2.html',
+	                    controller  : 'Route2Controller'
+	                }
+	            }
+	        })
+	    }])
+	  .controller('MyController', ['$scope', '$state', function ($scope, $state) {
 		$scope.greetMe = 'World (' + portletId + ')';
 		$scope.go = function(route){
-		    $location.path(route);
+		    $state.go(route);
 		}
 	  }])
 	  .controller('Route1Controller', ['$scope', function ($scope) {
--- modules/angular2-test/src/main/resources/META-INF/resources/view.jsp
+++ modules/angular2-test/src/main/resources/META-INF/resources/view.jsp
@@ -5,11 +5,11 @@
   <div ng-controller="MyController">
     <div>Hello {{greetMe}}!</div>
     <div>
-  		<a ng-click="go('/<portlet:namespace/>/route1')">Route 1</a>
-  		<a ng-click="go('/<portlet:namespace/>/route2')">Route 2</a>
+  		<a ng-click="go('route1')">Route 1</a>
+  		<a ng-click="go('route2')">Route 2</a>
   	</div>
   	
-  	<div ng-view></div>
+  	 <div ui-view="view">content</div>
   	
   </div>
 </div>
